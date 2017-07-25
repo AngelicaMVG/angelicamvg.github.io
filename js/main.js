c8fe16689ac183a50cf5ea146506a51f27129328
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-07-24 21:19:58 -0500

    portfolio version final

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var animationNombre = function() {
+  $("div[data-liffect]").each(function(i) {
+    $(this).attr("style", "-webkit-animation-delay:" + i * 300 + "ms;" +
+      "-moz-animation-delay:" + i * 300 + "ms;" +
+      "-o-animation-delay:" + i * 300 + "ms;" +
+      "animation-delay:" + i * 300 + "ms;");
+    if (i == $("div[data-liffect] ").size() - 1) {
+      $("div[data-liffect]").addClass("play")
+    }
+  });
+};
+
+var cargarProgressBar = function() {
+  var scroll = document.body.scrollTop;
+  if (scroll >= 792) {
+    $('.skill').show('slow');
+  } else if (scroll < 791) {
+    $('.skill').hide();
+  }
+}
+document.addEventListener("scroll", cargarProgressBar);
+
+$(document).ready(function() {
+  $(".button-collapse").sideNav({
+    menuWidth: 500, // Default is 300
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+  });
+  $('.parallax').parallax();
+  $('.modal').modal();
+
+  animationNombre();
+  cargarProgressBar();
+
+});
